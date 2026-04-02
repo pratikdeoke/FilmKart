1c46c039df2cc6f623e714e5a41d58e5f0bc04a7
pratikdeoke <deokepratik3@gmail.com>
2026-04-03 02:54:35 +0530

    redis.js modified - redis deployed successfully

--- services/booking-service/src/config/redis.js
+++ services/booking-service/src/config/redis.js
@@ -1,8 +1,16 @@
 import Redis from "ioredis";
 
-const redis = new Redis({
-  host: "127.0.0.1",
-  port: 6379
+const redis = new Redis(process.env.REDIS_URL, {
+  maxRetriesPerRequest: null,
+  enableReadyCheck: true,
 });
+
+// redis.on("connect", () => {
+//   console.log("✅ Redis connected");
+// });
+
+// redis.on("error", (err) => {
+//   console.error("❌ Redis error:", err.message);
+// });
 
 export default redis;
